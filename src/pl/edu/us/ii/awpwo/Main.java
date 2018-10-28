846aa18be1bdc9d687fda715a1dc9a8c7fc3227c
Student <Student@10.2.9.11>
2018-10-28 13:56:55 +0100

    JaRutkowski Warning fixed p. 10

--- src/pl/edu/us/ii/awpwo/Main.java
+++ src/pl/edu/us/ii/awpwo/Main.java
@@ -4,12 +4,12 @@ package pl.edu.us.ii.awpwo;
  * @author Student
  *
  */
-public class Main {
+class Main {
 	/**
 	 * Main point of app.
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Pies maksiu = new Pies();
 		Pies rocky = new Pitbull();
 		Pies cezar = new Bulterier();
